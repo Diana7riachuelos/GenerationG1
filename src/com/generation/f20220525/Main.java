858abe4a890bb5702c71c70394788922d670f6bf
Diana7riachuelos <lulumirpia@gmail.com>
2022-05-27 02:53:49 -0400

    Main.java

---
+++ src/com/generation/f20220525/Main.java
@@ -0,0 +1,28 @@
+package com.generation.f20220525;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num1 = 2;
+		int num2 = 4;
+		int num3 = 6;
+		int num4 = 8;
+		int num5 = 10;
+		int num6 = 12;
+		int num7 = 14;
+		int num8 = 16;
+		int num9 = 18;
+		int num10 = 20;
+		
+		int oddAddition = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+		System.out.println("La suma de los primeros 10 numeros impares es:" + oddAddition);
+		
+
+	}
+	public static boolean is0dd (int numero) {
+		
+	return numero % 2 == 0;
+		
+	}
+
+}
