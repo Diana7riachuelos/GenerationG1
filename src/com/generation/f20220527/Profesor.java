031962c186e0e7b9f20da6e6c2e5065e752dc751
Diana7riachuelos <lulumirpia@gmail.com>
2022-05-27 21:45:47 -0400

    Profesor.java

---
+++ src/com/generation/f20220527/Profesor.java
@@ -0,0 +1,15 @@
+package com.generation.f20220527;
+
+public class Profesor {
+	
+	String nombre;
+	String apellido;
+	
+	Boolean profesorJefe = true;
+	Boolean profesorAyudante = false;
+	
+	public static void impartirClases() {
+//la funcion modifica el atributo
+
+	}
+}
