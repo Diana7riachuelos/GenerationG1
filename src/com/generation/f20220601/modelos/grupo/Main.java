d512e211d4ccdc678a80108e3e5c78f34bbde92c
Diana7riachuelos <lulumirpia@gmail.com>
2022-06-01 22:15:05 -0400

    Main.java

---
+++ src/com/generation/f20220601/modelos/grupo/Main.java
@@ -0,0 +1,30 @@
+package com.generation.f20220601.modelos.grupo;
+
+import com.generation.f20220601.modelos.grupo6.TelefonoIpad;
+import com.generation.f20220601.modelos.grupo6.TelefonoOriginal;
+
+public class Main {
+	
+ public static void main(String[] args) {
+	 
+	 //instancias
+	 
+	 TelefonoOriginal telefonoOriginal = new TelefonoOriginal();
+	 telefonoOriginal.setAnio(1980);
+	 telefonoOriginal.setColor("gris");
+	 telefonoOriginal.setPantalla("Involucionada");
+	 System.out.println( telefonoOriginal.toString());
+	 
+    
+    
+    
+    
+	 
+	 
+	 
+	 
+	 
+	 
+	
+}
+}
