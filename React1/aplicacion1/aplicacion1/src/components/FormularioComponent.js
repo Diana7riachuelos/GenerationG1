675f32a9c451072a64dda78e716e140d08625317
Diana7riachuelos <105820606+Diana7riachuelos@users.noreply.github.com>
2022-07-15 18:05:31 -0400

    Add files via upload

---
+++ React1/aplicacion1/aplicacion1/src/components/FormularioComponent.js
@@ -0,0 +1,133 @@
+import React, {useState, useEffect} from "react";
+
+const initialValues =[
+{
+    key:'',
+    nombre:'',
+    apellido:'',
+    edad:'', 
+    password:''
+}
+]
+
+const FormularioComponent = ({usuarioAdd, usuarioEditado, usuarioEdit, setUsuarioEditado}) => {
+const [values, setValues] = useState(initialValues);
+const {key, nombre, apellido, edad, password}= values;
+
+useEffect( 
+    ()=>{
+    if(usuarioEditado !== null){
+        setValues(usuarioEditado)
+    }else{
+        setValues({
+        key:'',
+        nombre:'',
+        apellido:'',
+        edad:'', 
+        password:''
+        })
+    }
+    }
+    ,[usuarioEditado]);
+
+    {/*useEffect(accion que debe hacer, [estado del cual debe estar pendiente])*/}
+
+const handleInputChange=(e)=>{
+    
+    const changedFormValue ={
+    ...values, 
+    [e.target.name]:e.target.value
+      //key:key
+    }
+    setValues(changedFormValue)
+}
+
+const handleSubmit =(e)=>{
+    e.preventDefault();
+    if(usuarioEditado !== null){
+    usuarioEdit(values)
+    }else{
+    usuarioAdd(values)
+    }
+    
+}
+
+    return(
+    <form onSubmit={handleSubmit}>
+    <h1>{usuarioEditado ? 'Editar Usuario':'Crear Usuario'}</h1>
+      {/*usuarioEditado es nulo? si no muestra 'Editar usuario', si es nulo muestra 'Ingresar Usuario' */}
+        <div className="form-group">
+        <label>ID</label>
+        <input
+        type="text"
+        className="form-control"
+        id="key"
+        placeholder="Key"
+        value={key}
+        name='key'
+        onChange={handleInputChange}
+        />
+        <br/>
+        <label>Nombre</label>
+        <input
+        type="text"
+        className="form-control"
+        id="nombre"
+        placeholder="Nombre"
+        value={nombre}
+        name='nombre'
+        onChange={handleInputChange}
+        />
+        <br/>
+        </div>
+        <div className="form-group">
+        <label>Apellido</label>
+        <input
+        type="text"
+        className="form-control"
+        id="apellido"
+        placeholder="Apellido"
+        value={apellido}
+        name='apellido'
+        onChange={handleInputChange}
+        />
+        <br/>
+        </div>
+        <div className="form-group">
+        <label>Edad</label>
+        <input
+        type="text"
+        className="form-control"
+        id="edad"
+        placeholder="Edad"
+        value={edad}
+        name='edad'
+        onChange={handleInputChange}
+        />
+        <br/>
+        </div>
+        <div className="form-group">
+        <label>Password</label>
+        <input
+        type="password"
+        className="form-control"
+        id="password"
+        placeholder="Password"
+        value={password}
+        name='password'
+        onChange={handleInputChange}
+        ></input>
+        <br/>
+        </div>
+        <div>
+        <button type="submit" className="btn btn-outline-primary me-2">{usuarioEditado ? 'Editar':'Crear'}</button>
+        {usuarioEditado ? 
+        (<button type="button" className="btn btn-outline-warning" onClick={()=>{setUsuarioEditado(null)}}>Cancelar</button>):''}
+        
+        </div>
+        
+    </form>
+    );
+}
+
+export default FormularioComponent;
---
+++ React1/aplicacion1/aplicacion1/src/components/UsuarioComponent.js
@@ -0,0 +1,97 @@
+import React,{useState} from "react";
+import TarjetaComponent from "./TarjetaComponent";
+import FormularioComponent from "./FormularioComponent";
+
+{/* const usuario1 ={
+    nombre: 'Joseph',
+    apellido:'Joestar',
+    edad:'18'
+}
+const usuario2 ={
+    nombre: 'Josuske',
+    apellido:'Higashikata',
+    edad:'16'
+}*/}
+
+
+const initialUsuarios =[
+    {
+        key:0,
+        nombre: 'Joseph',
+        apellido:'Joestar',
+        edad:'18',
+        password: '1234'
+    },
+    {
+        key:1,
+        nombre: 'Josuske',
+        apellido:'Higashikata',
+        edad:'16',
+        password: '1234'
+    },
+    {
+        key:2,
+        nombre: 'Jonathan',
+        apellido:'Higashikata',
+        edad:'16',
+        password: '1234'
+    }
+]
+
+
+const UsuarioComponent = () => {
+
+    const [usuarios, setUsuarios] = useState(initialUsuarios);
+    {/* const state = useState(initialUsuarios);
+    state[0]=initialUsuarios
+    state[1]= funcion que nos permite hacer cambios */}
+    const [usuarioEditado, setUsuarioEditado]= useState(null);
+
+    const tarjetaDelete =(usuarioKey)=>{
+        const changeUsuarios = usuarios.filter(u => u.key !== usuarioKey)
+        setUsuarios(changeUsuarios)
+    }
+    const usuarioAdd =(usuario)=>{
+        const addUsuarios = [
+            ...usuarios,
+            usuario
+        ]
+        setUsuarios(addUsuarios)
+    }
+
+    const usuarioEdit=(usuarioEditado)=>{
+        const changeUsuarios = usuarios.map(usuario => (usuario.key === usuarioEditado.key ? usuarioEditado : usuario))
+        setUsuarios(changeUsuarios)
+    }
+
+    return(
+    <div className="container mt-4">
+    <div className="row">
+    <div className="col-8 ">
+        <h1>Lista</h1>
+        {
+            usuarios.map(u =>
+                <TarjetaComponent 
+                key={u.key} 
+                usuario={u} 
+                tarjetaDelete={tarjetaDelete}
+                setUsuarioEditado={setUsuarioEditado}/>)
+        }
+        
+    </div>
+    <div className="col">
+        <h1>Formulario</h1>
+        <FormularioComponent 
+        usuarioAdd={usuarioAdd} 
+        usuarioEditado={usuarioEditado}
+        usuarioEdit={usuarioEdit}
+        setUsuarioEditado={setUsuarioEditado}/>
+        
+    </div>
+    </div>
+</div>
+);
+
+}
+
+export default UsuarioComponent;
